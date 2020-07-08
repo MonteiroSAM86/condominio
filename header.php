84d8e16dcfceaf39583902e15bf435420ce36201
Hugo Monteiro <32527057+MonteiroSAM86@users.noreply.github.com>
2020-07-08 23:30:21 +0100

    update header

--- header.php
+++ header.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
     <header>
-        <nav>
-            <a href="#">
+        <nav class="nav-header-main">
+            <a class="header-logo" href="#">
                 <img src="img/logo.png" alt="logo">
             </a>
             <ul>
@@ -18,17 +19,19 @@
                 <li><a href="#">About me</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
-            <div>
-                <form action="includes/login.inc.php" method="post">
-                    <input type="text" name="mailuid" placeholder="Username/E-mail...">
-                    <input type="password" name="pwd" placeholder="Password...">
-                    <button type="submit" name="login-submit">Login</button>
-                </form>
-                <a href="signup.php">Signup</a>
-                <form action="includes/logout.inc.php" method="post">
-                    <button type="submit" name="logout-submit">Logout</button>
-                </form>
-            </div>
+        </nav>
+        <nav>
+        <div class="header-login">
+            <form action="includes/login.inc.php" method="post">
+                <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                <input type="password" name="pwd" placeholder="Password...">
+                <button type="submit" name="login-submit">Login</button>
+            </form>
+            <a href="signup.php">Signup</a>
+            <form action="includes/logout.inc.php" method="post">
+                <button type="submit" name="logout-submit">Logout</button>
+            </form>
+        </div>
         </nav>
     </header>
 </body>
