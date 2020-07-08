d65b8d011412bcf65c79a2156574b7f19dc79a8a
Hugo Monteiro <32527057+MonteiroSAM86@users.noreply.github.com>
2020-07-08 23:11:10 +0100

    Criação

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
+    <header>
+        <nav>
+            <a href="#">
+                <img src="img/logo.png" alt="logo">
+            </a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">About me</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div>
+                <form action="includes/login.inc.php" method="post">
+                    <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                    <input type="password" name="pwd" placeholder="Password...">
+                    <button type="submit" name="login-submit">Login</button>
+                </form>
+                <a href="signup.php">Signup</a>
+                <form action="includes/logout.inc.php" method="post">
+                    <button type="submit" name="logout-submit">Logout</button>
+                </form>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
