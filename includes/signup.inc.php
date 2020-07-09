3a8f851d87ae45f90dcf81c8c829209e8f00a4cd
Hugo Monteiro <32527057+MonteiroSAM86@users.noreply.github.com>
2020-07-09 18:42:20 +0100

    login

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,2 +1,32 @@
 <?php 
+if (isset($_POST['signup-submit'])) {
 
+    require 'dbh.inc.php';
+
+    $username = $_POST['uid'];
+    $email = $_POST['mail'];
+    $password = $_POST['pwd'];
+    $passwordRepeat = $_POST['pwd-repeat'];
+
+    if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)) {
+        header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail=".$email);
+        exit();
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) && (!preg_match("/^[a-zA-Z0-9]*$/" , $username)) {
+        header("Location: ../signup.php?error=invalidmailuid");
+        exit();
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: ../signup.php?error=invalidmail&uid=".$username);
+        exit();
+    }
+    elseif (!preg_match("/^[a-zA-Z0-9]*$/" , $username)) {
+        header("Location: ../signup.php?error=invaliduid&mail=".$email);
+        exit();
+    }
+    elseif ($password !== $passwordRepeat) {
+        header("Location: ../signup.php?error=passwordcheckuid=".$username."&mail=".$email);
+        exit();
+
+    }
+}
